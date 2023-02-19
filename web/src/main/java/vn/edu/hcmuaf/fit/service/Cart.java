70a68a3d94d6f1de5bbfb719563f73f4cfc8ce2e
khacnhuan14102002 <20130090@st.hcmuaf.edu.vn>
2023-02-19 21:44:12 +0700

    themvaogiohang

---
+++ web/src/main/java/vn/edu/hcmuaf/fit/service/Cart.java
@@ -0,0 +1,24 @@
+package vn.edu.hcmuaf.fit.service;
+
+import vn.edu.hcmuaf.fit.bean.products;
+
+import java.util.ArrayList;
+
+public class Cart {
+    public static ArrayList<products> dssanpham = new ArrayList<>();
+    public static ArrayList<products> giohang = new ArrayList<>();
+
+    public boolean addCart(String masp){
+        for(int i =0; i <dssanpham.size();i++){
+            if(dssanpham.get(i).getMasp().equals(masp)){
+                giohang.add(dssanpham.get(i));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static ArrayList<products> getGiohang() {
+        return giohang;
+    }
+}
