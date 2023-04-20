3a300084ffc1801bbe9407588c1530ec7959568d
Mai Thi <20130120@st.hcmuaf.edu.vn>
2023-04-20 20:13:09 +0700

    Thêm User

---
+++ web/src/main/java/vn/edu/hcmuaf/fit/controller/AddUserServlet.java
@@ -0,0 +1,42 @@
+package vn.edu.hcmuaf.fit.controller;
+
+import java.sql.Date;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import vn.edu.hcmuaf.fit.dao.UserDao;
+import vn.edu.hcmuaf.fit.bean.User;
+@WebServlet(name = "AddUserServlet", value = "/AddUserServlet")
+public class AddUserServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Lấy các thông tin từ request
+        String nameUser = request.getParameter("NamUser");
+        String emailUs = request.getParameter("EmailUs");
+        String pass = request.getParameter("Pass");
+        String phone = request.getParameter("Phone");
+        Integer roleus = Integer.parseInt(request.getParameter("RoleUs"));
+        Integer active = Integer.parseInt(request.getParameter("Active"));
+
+        // Tạo một đối tượng User mới với các thông tin vừa lấy
+        User user = new User();
+        user.setNameUser(nameUser);
+        user.setEmailUs(emailUs);
+        user.setPass(pass);
+        user.setPhone(phone);
+        user.setRoleUs(roleus);
+        user.setActive(active);
+
+        user.setRegistrationDate(new Date(System.currentTimeMillis())); // Lấy ngày hiện tại làm ngày đăng ký
+
+        // Thực hiện thêm user vào cơ sở dữ liệu
+        UserDao userDao = new UserDao();
+        userDao.addUser(user);
+
+        // Chuyển hướng về trang danh sách user
+        response.sendRedirect("QuanLyUser.jsp");
+    }
+}
