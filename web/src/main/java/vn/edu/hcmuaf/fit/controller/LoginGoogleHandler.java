c40d64063897503887ba0e7e5d60dfb2299634bc
khacnhuan14102002 <20130090@st.hcmuaf.edu.vn>
2023-05-21 16:16:58 +0700

    pagging quanly sanpham và xuat file excel

--- web/src/main/java/vn/edu/hcmuaf/fit/controller/LoginGoogleHandler.java
+++ web/src/main/java/vn/edu/hcmuaf/fit/controller/LoginGoogleHandler.java
@@ -1,51 +1,51 @@
-package vn.edu.hcmuaf.fit.controller;
-
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
-import java.io.IOException;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.fluent.Form;
-import org.apache.http.client.fluent.Request;
-import vn.edu.hcmuaf.fit.bean.Constants;
-import vn.edu.hcmuaf.fit.bean.GoogleUtils;
-import vn.edu.hcmuaf.fit.service.UserGoogleDto;
-import java.io.PrintWriter;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@WebServlet(name = "LoginGoogleHandler", value = "/LoginGoogleHandler")
-//@WebServlet(urlPatterns = { "/LoginGoogleHandler" })
-public class LoginGoogleHandler extends HttpServlet {
-
-        private static final long serialVersionUID = 1L;
-        public LoginGoogleHandler() {
-            super();
-        }
-        protected void doGet(HttpServletRequest request, HttpServletResponse response)
-                throws ServletException, IOException {
-            String code = request.getParameter("code");
-            if (code == null || code.isEmpty()) {
-                RequestDispatcher dis = request.getRequestDispatcher("login.jsp");
-                dis.forward(request, response);
-            } else {
-                String accessToken = GoogleUtils.getToken(code);
-                UserGoogleDto googlePojo = GoogleUtils.getUserInfo(accessToken);
-                request.setAttribute("id", googlePojo.getId());
-                request.setAttribute("name", googlePojo.getName());
-                request.setAttribute("email", googlePojo.getEmail());
-                RequestDispatcher dis = request.getRequestDispatcher("index.jsp");
-                dis.forward(request, response);
-            }
-        }
-        protected void doPost(HttpServletRequest request, HttpServletResponse response)
-                throws ServletException, IOException {
-            doGet(request, response);
-        }
-}
+//package vn.edu.hcmuaf.fit.controller;
+//
+//import com.google.gson.Gson;
+//import com.google.gson.JsonObject;
+//import javax.servlet.*;
+//import javax.servlet.http.*;
+//import javax.servlet.annotation.*;
+//import java.io.IOException;
+//import org.apache.http.client.ClientProtocolException;
+//import org.apache.http.client.fluent.Form;
+//import org.apache.http.client.fluent.Request;
+//import vn.edu.hcmuaf.fit.bean.Constants;
+//import vn.edu.hcmuaf.fit.bean.GoogleUtils;
+//import vn.edu.hcmuaf.fit.service.UserGoogleDto;
+//import java.io.PrintWriter;
+//import javax.servlet.RequestDispatcher;
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//@WebServlet(name = "LoginGoogleHandler", value = "/LoginGoogleHandler")
+////@WebServlet(urlPatterns = { "/LoginGoogleHandler" })
+//public class LoginGoogleHandler extends HttpServlet {
+//
+//        private static final long serialVersionUID = 1L;
+//        public LoginGoogleHandler() {
+//            super();
+//        }
+//        protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//                throws ServletException, IOException {
+//            String code = request.getParameter("code");
+//            if (code == null || code.isEmpty()) {
+//                RequestDispatcher dis = request.getRequestDispatcher("login.jsp");
+//                dis.forward(request, response);
+//            } else {
+//                String accessToken = GoogleUtils.getToken(code);
+//                UserGoogleDto googlePojo = GoogleUtils.getUserInfo(accessToken);
+//                request.setAttribute("id", googlePojo.getId());
+//                request.setAttribute("name", googlePojo.getName());
+//                request.setAttribute("email", googlePojo.getEmail());
+//                RequestDispatcher dis = request.getRequestDispatcher("index.jsp");
+//                dis.forward(request, response);
+//            }
+//        }
+//        protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//                throws ServletException, IOException {
+//            doGet(request, response);
+//        }
+//}
--- web/src/main/java/vn/edu/hcmuaf/fit/controller/ManageControl.java
+++ web/src/main/java/vn/edu/hcmuaf/fit/controller/ManageControl.java
@@ -3,6 +3,7 @@ package vn.edu.hcmuaf.fit.controller;
 import vn.edu.hcmuaf.fit.bean.category;
 import vn.edu.hcmuaf.fit.bean.products;
 import vn.edu.hcmuaf.fit.service.ManagerService;
+import vn.edu.hcmuaf.fit.service.StoreService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,10 +19,22 @@ public class ManageControl extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
 
-        ManagerService mana = new ManagerService();
-        List<products> listP = mana.getAllProduct();
-        List<category> listC = mana.getAllCategory();
-
+        ManagerService mana1 = new ManagerService();
+        StoreService mana =new StoreService();
+        String indexPage = request.getParameter("page");
+        if (indexPage == null) {
+            indexPage = "1";
+        }
+        int index = Integer.parseInt(indexPage);
+       int count = mana.gettotalpro();
+        int endPage = count / 12;
+        if (count % 12 != 0) {
+            endPage++;
+        }
+        List<products> listP = mana.paging(index);
+        List<category> listC = mana1.getAllCategory();
+        request.setAttribute("tag", index);
+        request.setAttribute("endP", endPage);
         request.setAttribute("listC", listC);
         request.setAttribute("listP", listP);
         request.getRequestDispatcher("managerProduct.jsp").forward(request, response);
